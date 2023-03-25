b6257c9f1c9548f9af90eb42864b492b517bf56b
Tim Schindler <tim@iosharp.com>
2023-03-25 20:47:05 +0100

    remove checkpoint used for debugging

--- PAMVaultExample.ps1
+++ PAMVaultExample.ps1
@@ -4,9 +4,9 @@ New-LabDefinition -Name PAMVault -DefaultVirtualizationEngine HyperV -VmPath C:\
 
 $PAMVaultRoleProperties = @{
     InstallationArchivePath = 'C:\LabSources\CyberArkInstallFiles\Server-Rls-v13.0.zip'
-    OperatorKeysFolder = 'C:\LabSources\CyberArkInstallFiles\DemoOperatorKeys'
-    MasterKeysFolder = 'C:\LabSources\CyberArkInstallFiles\DemoMasterKeys'
-    LicensePath = 'C:\LabSources\CyberArkInstallFiles\nfr_license.xml'
+    OperatorKeysFolder      = 'C:\LabSources\CyberArkInstallFiles\DemoOperatorKeys'
+    MasterKeysFolder        = 'C:\LabSources\CyberArkInstallFiles\DemoMasterKeys'
+    LicensePath             = 'C:\LabSources\CyberArkInstallFiles\nfr_license.xml'
 }
 $PAMVaultRole = Get-LabPostInstallationActivity -CustomRole PAMVault -Properties $PAMVaultRoleProperties
 
@@ -16,11 +16,8 @@ Add-LabMachineDefinition -Name $VaultName -PostInstallationActivity $PAMVaultRol
 Install-Lab -BaseImages -NetworkSwitches -VMs -Domains -NoValidation
 
 # Start the Vault VM and wait for it to be ready
-Write-ScreenInfo "Starting virtual machines"
+Write-ScreenInfo 'Starting virtual machines'
 Start-LabVM -All -Wait
-
-Checkpoint-LabVM -All -SnapshotName "Pre-install"
 
 # Perform the Vault installation. The hardening part of the installation will kill the ability to use WinRM so it will timeout and throw an error. We want to ignore that error.
 Invoke-LabCommand -ComputerName $VaultName -PostInstallationActivity -ActivityName 'CyberArk Vault installation' -ErrorAction SilentlyContinue
-
