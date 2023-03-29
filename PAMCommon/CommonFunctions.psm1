76f8e7b1398039487a5ff8e012a06f229b7e439c
Tim Schindler <tim@iosharp.com>
2023-03-29 21:11:38 +0200

    correctly checks the installed version on the lab vm. fixes #6

--- PAMCommon/CommonFunctions.psm1
+++ PAMCommon/CommonFunctions.psm1
@@ -26,9 +26,9 @@ function Install-PAMCommonPreRequisites {
 
     if ($VisualCRedistributable32) {
         $DesiredBuild = 31938 # 2022
-        $InstalledBuild = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\14.0\VC\Runtimes\X86' -Name Bld
+        $InstalledBuild = Get-LabRegistryKeyValue -ComputerName $ComputerName -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\14.0\VC\Runtimes\X86' -Name Bld -ErrorAction SilentlyContinue
 
-        if ($InstalledBuild -ne $DesiredBuild -or $null -eq $InstalledBuild) {
+        if ($InstalledBuild -lt $DesiredBuild -or $null -eq $InstalledBuild) {
             Write-ScreenInfo 'Installing Visual C++ Redistributable 2022 x86'
             $VisualCRedistX86 = Get-LabInternetFile -Uri 'https://aka.ms/vs/17/release/vc_redist.x86.exe' -Path $CyberArkInstallFolder -PassThru
             Install-LabSoftwarePackage -ComputerName $ComputerName -Path $VisualCRedistX86.FullName -CommandLine '/Q'
@@ -37,9 +37,9 @@ function Install-PAMCommonPreRequisites {
 
     if ($VisualCRedistributable64) {
         $DesiredBuild = 31938 # 2022
-        $InstalledBuild = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\14.0\VC\Runtimes\X86' -Name Bld
+        $InstalledBuild = Get-LabRegistryKeyValue -ComputerName $ComputerName -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\VisualStudio\14.0\VC\Runtimes\X64' -Name Bld -ErrorAction SilentlyContinue
 
-        if ($InstalledBuild -ne $DesiredBuild -or $null -eq $InstalledBuild) {
+        if ($InstalledBuild -lt $DesiredBuild -or $null -eq $InstalledBuild) {
             Write-ScreenInfo 'Installing Visual C++ Redistributable 2022 x64'
             $VisualCRedistX64 = Get-LabInternetFile -Uri 'https://aka.ms/vs/17/release/vc_redist.x64.exe' -Path $CyberArkInstallFolder -PassThru
             Install-LabSoftwarePackage -ComputerName $ComputerName -Path $VisualCRedistX64.FullName -CommandLine '/Q'
@@ -47,10 +47,10 @@ function Install-PAMCommonPreRequisites {
     }
 
     if ($DotNetFramework48) {
-        $DesiredRelease = 528049 # .NET Framework 4.8
-        $InstalledRelease = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full' -Name Release
+        $DesiredRelease = 528049 # .NET Framework 4.8.0
+        $InstalledRelease = Get-LabRegistryKeyValue -ComputerName $ComputerName -Path 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full' -Name Release -ErrorAction SilentlyContinue
 
-        if ($InstalledRelease -ne $DesiredRelease -or $null -eq $InstalledRelease) {
+        if ($InstalledRelease -lt $DesiredRelease -or $null -eq $InstalledRelease) {
             Write-ScreenInfo 'Installing .NET Framework 4.8'
             $RequiresRestart = $true
             $DotNetFramework48 = Get-LabInternetFile -Uri 'https://go.microsoft.com/fwlink/?linkid=2088631' -Path $CyberArkInstallFolder -PassThru
@@ -62,4 +62,19 @@ function Install-PAMCommonPreRequisites {
         Write-ScreenInfo 'Restarting as pre-requisites require a restart'
         Restart-LabVM -ComputerName $ComputerName -Wait
     }
+}
+
+function Get-LabRegistryKeyValue {
+    param (
+        $ComputerName,
+        $Path,
+        $Name
+    )
+
+    $Value = Invoke-LabCommand -ComputerName $ComputerName -ScriptBlock {
+        $ErrorActionPreference = 'SilentlyContinue'
+        Get-ItemPropertyValue -Path $args[0] -Name $args[1]
+    } -ArgumentList $Path, $Name -NoDisplay -PassThru
+
+    $Value
 }
